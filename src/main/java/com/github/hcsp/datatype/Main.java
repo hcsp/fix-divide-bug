cf4bdc6ae7fb581f78f01e9fff9f59af5a801327
Administrator <908380434@qq.com>
2019-06-06 00:11:20 +0800

    修复一个除法Bug

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -6,10 +6,10 @@ public class Main {
     // 请修复此问题，让2/1的结果为正确的0.5
     // 不要修改方法的参数类型
     public static double divide(int a, int b) {
-        return a / b;
+        return (float) a / b ;
     }
 
     public static void main(String[] args) {
-        System.out.println("2/1=" + divide(2, 1));
+        System.out.println("3/2=" + divide(3, 2));
     }
 }
