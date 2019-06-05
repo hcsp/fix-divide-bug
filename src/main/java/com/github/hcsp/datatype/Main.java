ff8fa77e6e0b1a3f30d2837bc38ad20cc874bb93
Bo Zhang <bo@gradle.com>
2019-06-06 07:45:20 +0800

    Fix description

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -2,14 +2,14 @@ package com.github.hcsp.datatype;
 
 public class Main {
     // 我们希望实现一个除法，但是这里的结果有明显的错误
-    // 例如 2/1 == 1.0
-    // 请修复此问题，让2/1的结果为正确的0.5
+    // 例如 3/2 == 1.0
+    // 请修复此问题，让3/2的结果为正确的1.5
     // 不要修改方法的参数类型
     public static double divide(int a, int b) {
         return a / b;
     }
 
     public static void main(String[] args) {
-        System.out.println("2/1=" + divide(2, 1));
+        System.out.println("3/2=" + divide(3, 2));
     }
 }
