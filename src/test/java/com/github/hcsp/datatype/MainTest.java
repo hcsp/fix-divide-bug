322ef1a1a1ee7037807facab9ea6f716a8b87c61
yaohengfeng <1065435454@qq.com>
2019-12-07 18:08:09 +0800

    修改

--- src/test/java/com/github/hcsp/datatype/MainTest.java
+++ src/test/java/com/github/hcsp/datatype/MainTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 public class MainTest {
     @Test
     public void test() throws NoSuchMethodException {
-            Assertions.assertTrue(Math.abs(1d / 3d - Main.divide(1, 3)) < 1e-5);
+        Assertions.assertTrue(Math.abs(1d / 3d - Main.divide(1, 3)) < 1e-5);
         Assertions.assertTrue(Math.abs(1d / 2d - Main.divide(1, 2)) < 1e-5);
         Main.class.getDeclaredMethod("divide", int.class, int.class);
     }
