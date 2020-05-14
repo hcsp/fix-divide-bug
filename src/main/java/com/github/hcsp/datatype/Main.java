2f972a7ceeac064805740d0ac94fbce2955ee3ca
Administrator <756433556@qq.com>
2020-05-14 22:54:24 +0800

    修复除法中的bug

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -4,7 +4,7 @@ public class Main {
     // 我们希望实现一个除法，但是这里的结果有明显的错误
     // 例如 3/2 == 1.0
     // 请修复此问题，让3/2的结果为正确的1.5
-    // 不要修改方法的参数类型
+    //   不要修改方法的参数类型
     public static double divide(double a, double b) {
 
         return a / b;
